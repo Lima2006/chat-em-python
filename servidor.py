9131d6732a874e88ac4856ce607811b87edebac9
lucaskumagae <lucas.kumagae@gmail.com>
2024-11-08 02:33:09 -0300

    arquivo do servidor do chat

--- servidor.py
+++ servidor.py
@@ -1,40 +1,71 @@
 import socket as sock
 import threading
 
+lista_clientes = {}
+
 
 # Função para recebimento de mensagem do cliente
 def recebe_dados(sock_cliente, endereco):
     nome = sock_cliente.recv(50).decode()
+    lista_clientes[nome] = sock_cliente
     print(f"Conexão bem sucedida com {nome} via endereço {endereco}")
+    broadcast(f"{nome} entrou no servidor", "")
     while True:
         try:
             mensagem = sock_cliente.recv(1024).decode()
-            if not mensagem:
-                print(f"{nome} saiu do chat.")
+            if not mensagem or mensagem == "/sair":
+                print(f"{nome} saiu do chat.", "")
                 break
-            print(f"{nome} >> {mensagem}")
+            elif mensagem == "/clientes":
+                sock_cliente.send(
+                    f"Clientes conectados: {', '.join(lista_clientes.keys())}".encode()
+                )
+            elif mensagem.startswith("/"):
+                unicast(mensagem, nome, sock_cliente)
+            else:
+                print(f"{nome} >> {mensagem}")
+                broadcast(mensagem, nome)
         except:
-            print("Erro ao receber mensagem... fechando")
+            print(f"Erro, encerrando conexão com o cliente {nome}.")
             break
-        sock_cliente.close()
+    broadcast(f"{nome} saiu do chat.", "")
+    remover(nome)
+    sock_cliente.close()
 
 
-def broadcast(lista_clientes):
-    pass
+# Função para mandar mensagem para todos, menos para quem mandou a mensagem
+# remetente é o socket de quem mandou a mensagem
+def broadcast(mensagem, remetente):
+    for nome, cliente in lista_clientes.items():
+        if nome != remetente:
+            try:
+                cliente.sendall(f"{remetente} >> {mensagem}".encode())
+            except:
+                remover(nome)
 
 
-def unicast(cliente):
-    pass
+# Função para mandar mensagem privada para outro cliente
+def unicast(mensagem, nome, sock_cliente):
+    try:
+        destinatario, texto = mensagem[1:].split(" ", 1)
+        # Envia a mensagem apenas para o destinatário
+        if destinatario in lista_clientes:
+            lista_clientes[destinatario].send(f"Mensagem de {nome}: {texto}".encode())
+        else:
+            sock_cliente.send("Destinatário não encontrado.".encode())
+    except ValueError:
+        sock_cliente.send("Formato inválido. Use '/destinatário mensagem'".encode())
+
+#remove nomes da lista_clientes
+def remover(nome):
+    if nome in lista_clientes:
+        lista_clientes[nome].close()
+        del lista_clientes[nome]
 
 
-def remover(cliente):
-    pass
-
-
-HOST = "127.0.0.1"
+HOST = "26.32.208.255"
 PORTA = 9999
 
-lista_clientes = []
 sock_server = sock.socket(sock.AF_INET, sock.SOCK_STREAM)
 
 # Fazemos o bind -> LINK do IP:PORTA
@@ -46,7 +77,6 @@ print(f"O servidor {HOST}:{PORTA} está aguardando conexões...")
 # Vamos criar um loop para o servidor acertar várias conexões
 while True:
     sock_conn, ender = sock_server.accept()
-    print(f"Conexão com sucesso do cliente: {ender}")
     # Conexão com sucesso, vamos receber dados
     # Criamos uma thread para a função recebe_dados(sock_conn, ender)
     thread_cliente = threading.Thread(target=recebe_dados, args=[sock_conn, ender])
