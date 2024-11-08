bf48491b59dd11c8f4d23bde304619edd670f56b
HenriqueSzlachta <szlachtahenrique@gmail.com>
2024-11-08 02:26:37 -0300

    Add files via upload

--- cliente.py
+++ cliente.py
@@ -1,29 +1,37 @@
 import socket as sock
+import threading
 
 
-def recebe_dados(sock_cliente, endereco):
+def recebe_dados(sock_cliente):
     while True:
-        sock_conn, ender = sock_cliente.accept()
-        print(f"Conexão com sucesso do cliente: {ender}")
-        while True:
-            try:
-                mensagem = sock_conn.recv(1024).decode()
-                print(f"Cliente >> {mensagem}")
-            except:
-                print("Erro ao receber mensagem... fechando")
-                sock_conn.close()
-                break
+        try:
+            mensagem = sock_cliente.recv(1024).decode()
+            print("\n" + mensagem)
+        except:
+            print("Erro, encerrando conexão com o servidor")
+            sock_cliente.close()
+            break
 
 
-HOST = "127.0.0.1"
+HOST = "26.32.208.255"
 PORTA = 9999
 
 socket_cliente = sock.socket(sock.AF_INET, sock.SOCK_STREAM)
 
 socket_cliente.connect((HOST, PORTA))
 print(5 * "-" + " Chat Iniciado " + 5 * "-")
-username = input("Insira o seu nome: ")
+while True:
+    username = input("Insira o seu nome: ")
+    if " " in username:
+        print("Nome inválido, não é permitido haver espaços no seu nome")
+        continue
+    else:
+        break
+    
 socket_cliente.sendall(username.encode())
+
+thread_receber = threading.Thread(target=recebe_dados, args=[socket_cliente])
+thread_receber.start()
 
 while True:
     mensagem = input("")
